7966a6f33e46581e943a8ebd9ccc043e841b773b
SchizoDuckie <SchizoDuckie@ca037e20-5248-0410-89ba-c7287fb9a197>
2009-01-21 14:48:40 +0000

    Latest version of Pork.dbObject
    -Rewritten dbconnection class to use true database abstraction via a dbConnectionAdapter (currently includes SQLite and MySQL)
    -Updated dbObject to use new abstractions.
    -Now uses a settings class for settings.ini instead of a php file (for easer maintenance) 
    -Updated dbObject to use new settings.ini
    -Includes a custom logger class that handles your PHP / Pork errors and logs them to a special database
    -Updated dbObject to use new logger class. It's now way easier to see when you do something wrong
    - New FindCount and SearchCount functions to execute select count * where xx functions
    - Optimized querybuilder and dbObject to be able to connect to a specific database from settings.ini.

--- class.dbconnection.php
+++ class.dbconnection.php
@@ -1,224 +1,668 @@
-<?
+<?php
+
+/**
+ *
+ *	By Jelle Ursem
+ *	
+ *	Ultra-simple database abstraction class.
+ *	Reads settings from a properties file and takes care of executing queries.
+ *	You can easily extend this class to add new database types.	
+ *	see http://code.google.com/p/pork-dbobject/ for more info
+ *
+ *	@package Pork
+ */
 
 
 /**
-
-	Ultra-simple database abstraction class.
-	Reads settings from a properties file and takes care of executing queries.
-	You can easily extend this class to add new database types.
-
-*/
-
-
+ * dbConnection class
+ * Handles database connections and querying/inserting/removal of rows.
+ * 
+ * @package Pork
+ * @author Jelle Ursem
+ * @copyright Jelle Ursem 2009
+ * @version 2.0
+ * @access public
+ */
 class dbConnection
-{
-	var $num_rows, $affected_rows, $connection, $result, $lastquery, $error, $output, $username, $password, $database, $queries, $func, $host;
-
-		
+{	
+	 public  $adapter, $insertID, $log, $debug;
 	/**
+	 * dbConnection::__construct()
 	 * Reads settings from the default settings file and creates the connection.
+	 * @param String $useAlternative Alternative settings file
 	 */
-	function __construct($useAlternative = false)
-	{
-		$this->readSettings( (!$useAlternative) ? dirname(__FILE__).'/../settings/dbsettings.php' : $useAlternative);
-		$this->connect();
-	}
 	
-	/*
-     * Read a file line-by-line to fetch the settings.
-	 */
-	function readSettings($file)
+	function __construct($instanceName='Database')
 	{
-		$input = file_get_contents($file);
-		$file = explode("\n", $input );
-		for ($i=2; $i<sizeof($file) -1; $i++)
-		{
-			$property = explode ("=", $file[$i]);
-			$prop = trim($property[0]);
-			$this->$prop = trim($property[1]);
-		}
+		$this->adapter = $this->getAdapter(Settings::Load()->Get($instanceName));
+		$this->debug = Settings::Load()->Get($instanceName, 'debug') === '1';
 	}
 
-	/* 
-	 * Singleton functionality 
+	/**
+	 * dbConnection::getInstance()	 
+	 * Singleton functionality.
+	 * Creates a static instance.
+	 * Usage: DbConnection::getInstance($name)->fetchAll("show tables");
+	 * @return DbConnection instance
 	 */
-	public static function getInstance()
+	public static function getInstance($instanceName='Database')
     {
-		static $instance;
-		if (!isset($instance)) 
+		static $instances = array();
+		if (!array_key_exists($instanceName, $instances)) 
 		{
-		     $c = __CLASS__;
-		     $instance = new $c;
-        }
-        return $instance;
+		   $instances[$instanceName] = new dbConnection($instanceName);
+	   }
+	  return $instances[$instanceName];
     }
 
+
 	/**
+	 * dbConnection::getAdapter()
+	 *
+	 * Returns the correct adapter class for the current database type.
+	 *
+	 */
+	private function getAdapter($connectInfo)
+	{
+
+		if(class_exists($connectInfo['dbtype']."Adapter"))
+		{
+			$adapterclass = "{$connectInfo['dbtype']}Adapter";
+			$adapter = new $adapterclass($connectInfo);
+		}
+		else
+		{
+			die("Adapter not found for database connection {$connectInfo->dbtype}");
+		}
+		return($adapter);
+	}
+
+
+	/**
+	 * dbConnection::connect()
 	 * Creates the actual connection
+	 * @return bool did the connection succeed
 	 */
 	function connect()
 	{
-		switch ($this->dbtype)
+		$this->connection = $this->adapter->connect();
+
+		if ($this->connection)	
 		{
-			case 'mysql':
-				$this->connection = mysql_connect($this->host, $this->username, $this->password);
-				if ($this->connection)
-				{
-					mysql_select_db($this->database, $this->connection);
-					return true;
-				}
-			break;
+			return true;
+		}
+		else
+		{
+			logger::getInstance()->LogEmail(get_class($this->adapter).' : could not connect');
 		}
 		return false;
 	}
 
 
-	
+	function escapeValue($value)
+	{
+		return($this->adapter->escapeValue($value));
+	}
+
 	/**
+	 * dbConnection::numrows()
 	 * Find out the number of rows returned
+	 * @return int Number of rows
 	 */
 	function numrows()
 	{
-		switch ($this->dbtype)
-		{
-			case 'mysql':
-				return @mysql_num_rows($this->result);
-			break;
-		}		
+		return($this->adapter->numRows($this->result));
 	}
 
 		
 	/**
+	 * dbConnection::query()
 	 * Execute the passed query on the database and determine if insert_id or affected_rows or numrows has to be called.
+	 * @param String $query Query to be executed.
+	 * @returns mixed ID if inserted row, false on error
 	 */
 	function query($query)
 	{
-		$this->queries[]= $query;
-		$this->insertID = 0;
-		$this->lastQuery = $query;
-		if ($this->dbtype == 'mysql') { mysql_query("use {$this->database}", $this->connection); }
-		$this->result = mysql_query($this->lastQuery, $this->connection);
-		$this->error = mysql_error($this->connection);
-		$query = strtolower($this->lastQuery);
-		if (empty($this->error))
-		{
-			if (strpos($query, 'insert') !== false)
-			{
-				$this->insertID = mysql_insert_id($this->connection);
-				$this->num_rows = 0;
-			}
-			elseif (strpos($query, 'delete') !== false || strpos($query, 'replace') !== false || strpos($query, 'update') !== false)
-			{
-				$this->affected_rows = mysql_affected_rows($this->connection); 
-				$this->num_rows = 0;
-			}
-			else
-			{
-				$this->num_rows = $this->numrows();
-				$this->affected_rows = 0;
-			}
-			if (!empty($this->insertID)) { return ($this->insertID); }
+		if($this->adapter->connection == false) {
+			logger::getInstance()->LogEmail('Not connected');
+			return false;
 		}
-		else
+		if($this->debug) Logger::getInstance()->log[]= "executing: {$query}";
+				
+		$result = $this->adapter->query($query);
+		
+		if(!$result)
 		{
-		 die($this->error."\nWhile executing query: \n{$query}");
-		 return false;
+			$error = $this->adapter->getError();
+			if($error != '' && $this->adapter->connection != false) Logger::Warn("Error '{$error}' during execution of query {$query}");
+			return false;
 		}
+		
+		$query = trim(strtolower($query));
+
+		$firstpart = substr($query, 0, 6);
+		$this->insertID = false;
+		$this->affected = false;
+		$this->numrows = false;
+		switch($firstpart)
+		{
+				case 'insert':
+					$this->insertID = $this->adapter->getInsertID();
+				break;
+				case 'delete':
+				case 'replac':
+				case 'update':
+					$this->affected = $this->adapter->numAffected();
+				break;
+				case 'select':
+					$this->numrows = $this->adapter->numRows();
+				break;
+		}
+		if ($this->insertID != false) { return ($this->insertID); }
+
 		return true;
 		
 	}
 
 	/**
-	 * Execute the query and return result # 0
+	 * dbConnection::fetchOne()
+	 * Execute the query and return result # 0.
+	 * If no query is passed it will use the previous result.
+	 * @param $query optional query to execute. 
+	 * @returns String $output
 	 */
-	function fetchOne($query='')
+	function fetchOne($query)
 	{
-		if (!empty($query)) $okay = $this->query($query);
-		if($okay !== false)
-		{
-			$this->output = ($this->num_rows > 0) ? mysql_result($this->result, 0) : '';
-		}
-		return($this->output);
+		return ($this->adapter->fetchOne($query));
 	}
 
 	/**
+	 * dbConnection::fetchAll()
 	 * Execute the passed query and fetch a multi-dimensional array of results using $func
+	 * If no query is passed it will use the previous result.
+	 * @param $query optional query to execute. 
+	 * @param $func function to use. Can use mysql_fetch_array or mysql_fetch_object or mysql_fetch_assoc at will.
+	 * @returns Array|Object $output multi dimensional array of output.
 	 */
-	function fetchAll($query='', $func=false)
+	function fetchAll($query=false, $type='assoc')
 	{
-		$output = array();
-		if (!empty($query)) $okay = $this->query($query);
-		if ($okay !== false)
+		return($this->adapter->fetchAll($query, $type));
+	}
+
+	/** 
+	 * dbConnection::fetchRow()
+	 * Execute the passed query and fetch only one row of results using $func
+	 * If no query is passed it will use the previous result.
+	 * @param $query optional query to execute. 
+	 * @param $func function to use. Can use array or object or assoc at will.
+	 * @returns Array|Object $output multi dimensional array of output.
+	 */
+	function fetchRow($query=false, $type='assoc')
+	{
+		return ($this->adapter->fetchRow($query, $type));
+	}
+
+	/**
+	 * dbConnection::setDatabase()
+	 * 
+	 * @param mixed $val
+	 * @return
+	 */
+	function setDatabase($val)
+	{
+		$this->adapter->setDatabase($val);
+	}
+
+	/**
+	 * dbConnection::getError()
+	 * Returns the last error from the database connection. 
+	 * @return string error
+	 */
+	function getError()
+	{
+		return($this->adapter->getError());
+	}
+
+
+	/**
+	 * dbConnection::getQueries()
+	 * Returns an array with executed SQL queries.
+	 * @return array $queries
+	 */
+	function getQueries()
+	{
+		return($this->adapter->queries);
+	}
+
+
+	/**
+	 * dbConnection::tableExists()
+	 * @param string $table talble to check if exists
+	 * @return boolean exists
+	 */	
+	function tableExists($table)
+	{
+		return($this->adapter->tableExists($table));
+	}
+
+
+}
+
+/**
+ *  dbConnectionAdapter interface.
+ *
+ *  Defines all the functions a database adapter should have to be working with Pork.dbObject
+ * 
+ * @package Pork
+ * @author Jelle Ursem 
+ * @copyright Jelle Ursem 2009
+ * @version 1.0
+ * @access public
+ */
+interface dbConnectionAdapter 
+{  
+	public function __construct($info);
+	public function connect($host, $username,$password);
+	public function escapeValue($value);
+	public function fetchOne($query=false);
+	public function fetchRow($query=false, $type='assoc');
+	public function fetchAll($query=false, $type='assoc');	
+	public function getError();    
+	public function getInsertID();
+	public function numRows();
+	public function numAffected();
+	public function query($query);
+	public function selectDatabase($db);	
+	public function tableExists($table);
+}
+
+/**
+ * MySQLAdapter
+ * 
+ * @package Pork
+ * @author Jelle Ursem 
+ * @copyright Jelle Ursem 2009
+ * @version 1.0
+ * @access public
+ */
+class MySQLAdapter implements dbConnectionAdapter
+{
+	public $connection;
+	public $result;
+	public $database;
+	public $queries;
+
+	/**
+	 * MySQLAdapter::__construct()
+	 * 
+	 * @param mixed $info
+	 * @return void
+	 */
+	public function __construct($info)
+	{
+		$this->database = $info['database'];
+		$this->connection = $this->connect($info['host'], $info['username'], $info['password']);
+		$this->queries = array();
+	}
+
+	/**
+	 * MySQLAdapter::escapeValue()
+	 * 
+	 * @param mixed $value
+	 * @return
+	 */
+	public function escapeValue($value)
+	{
+		return mysql_real_escape_string($value);
+	}
+
+	/**
+	 * MySQLAdapter::connect()
+	 * 
+	 * @param mixed $host
+	 * @param mixed $username
+	 * @param mixed $password
+	 * @return
+	 */
+	public function connect($host, $username, $password)
+	{	
+		return mysql_connect($host, $username, $password);
+	}
+
+	/**
+	 * MySQLAdapter::query()
+	 * 
+	 * @param mixed $query
+	 * @return
+	 */
+	public function query($query)
+	{
+		$this->queries[] = $query;
+		if(!$this->selectDatabase($this->database)) return false;
+		$this->result = mysql_query($query, $this->connection);
+		return($this->result);
+	}
+
+	/**
+	 * MySQLAdapter::fetchOne()
+	 * 
+	 * @param bool $query
+	 * @return
+	 */
+	public function fetchOne($query=false)
+	{	
+		if($query != false) $this->query($query);
+		if($this->result != false && mysql_num_rows($this->result) > 0 && mysql_num_fields($this->result) > 0)
 		{
-			$func = ($func != false) ? $func : $this->func;
+			return( mysql_result($this->result,0) );
+		}
+		return false;
+	}
+
+	/**
+	 * MySQLAdapter::fetchRow()
+	 * 
+	 * @param bool $query
+	 * @param string $type
+	 * @return
+	 */
+	/**
+	 * MySQLAdapter::fetchRow()
+	 * 
+	 * @param bool $query
+	 * @param string $type
+	 * @return
+	 */
+	public function fetchRow($query=false, $type='assoc')
+	{
+		if($query != false) $this->query($query);
+		if($this->result != false)
+		{
+			$func = "mysql_fetch_{$type}";
+			return($func($this->result));
+		}
+		return false;
+	}
+
+	/**
+	 * MySQLAdapter::fetchAll()
+	 * 
+	 * @param bool $query
+	 * @param string $type
+	 * @return
+	 */
+	public function fetchAll($query=false, $type='assoc')
+	{
+		if($query != false) $this->query($query);
+		if($this->result !== false)
+		{
+			$func = "mysql_fetch_{$type}";
+			$output = array();	
 			while ($row = $func($this->result))
 			{
 				$output[] = $row;
 			}
-			$this->output = $output;
+			return $output;
 		}
-		return($output);
+		return false;
 	}
-
-	/** 
-	 *	Execute the passed query and fetch only one row of results using $func
+	
+	/**
+	 * MySQLAdapter::getInsertID()
+	 * 
+	 * @return
 	 */
-	function fetchRow($query='', $func=false)
+	public function getInsertID()
 	{
-		$output = array();
-		if (!empty($query)) $okay = $this->query($query);
-		if ($okay !== false)
-		{
-			$func = ($func != false) ? $func : $this->func;
-			if ($row = $func($this->result))
-			{
-				$output = $row;
-			}
-			$this->output = $output;
-
-		}
-		return($output);	
+		return mysql_insert_id($this->connection);
 	}
 
-
-	function setPassword($val)
+	/**
+	 * MySQLAdapter::numRows()
+	 * 
+	 * @return
+	 */
+	public function numRows()
 	{
-		$this->password = $val;
+		return ($this->result) ? mysql_num_rows($this->result) : 0;
 	}
 
-	function setUsername($val)
+	/**
+	 * MySQLAdapter::numAffected()
+	 * 
+	 * @return
+	 */
+	public function numAffected()
 	{
-		$this->username = $val;
+		return ($this->result) ? @mysql_affected_rows($this->result) : 0;
 	}
 
-	function setHost($val)
+	/**
+	 * MySQLAdapter::selectDatabase()
+	 * 
+	 * @param mixed $db
+	 * @return
+	 */
+	public function selectDatabase($db)
 	{
-		$this->host = $val;
+		return mysql_select_db($db, $this->connection);
 	}
 
-	function setDatabase($val)
+	/**
+	 * MySQLAdapter::tableExists()
+	 * 
+	 * @param mixed $table
+	 * @return
+	 */
+	public function tableExists($table)
 	{
-		$this->database = $val;
-		if ($this->connection)
-		{
-			mysql_select_db($val, $this->connection);
-		}
+		$input = $this->fetchOne("SHOW TABLES FROM {$this->database} LIKE '{$table}'");
+		return($input != false);
 	}
 
-	function setDbType($type)
+	/**
+	 * MySQLAdapter::getError()
+	 * 
+	 * @return
+	 */
+	public function getError()
 	{
-		if ($type == 'mysql')
-		{
-			$this->dbtype = $type;
-		}
+		return mysql_error($this->connection);
 	}
-
-	function getError()
-	{
-		return($this->error);
-	}
-
 }
 
 
-?>
+/**
+ * SQLiteAdapter
+ *
+ * @package Pork
+ * @author Jelle Ursem 
+ * @copyright Jelle Ursem 2009
+ * @version 1.0
+ * @access public
+ */
+class SQLiteAdapter implements dbConnectionAdapter
+{
+	public $connection, $result,$database,$queries;
+
+	/**
+	 * SQLiteAdapter::__construct()
+	 * 
+	 * @param mixed $info
+	 * @return void
+	 */
+	public function __construct($info)
+	{
+		$error = false;
+		$this->database = $info['database'];
+		$this->connection = $this->connect($info['database'], $info['mode'],$error);
+		if($error != false)
+		{
+			echo print_array($error, 'SQLITE connection error');
+		}
+	}
+
+	/**
+	 * SQLiteAdapter::escapeValue()
+	 * 
+	 * @param mixed $value
+	 * @return
+	 */
+	public function escapeValue($value)
+	{
+		return sqlite_escape_string($value);
+	}
+
+
+	/**
+	 * SQLiteAdapter::connect()
+	 * 
+	 * @param mixed $host
+	 * @param mixed $username
+	 * @param mixed $password
+	 * @return
+	 */
+	public function connect($host, $username, $password)
+	{	
+		return sqlite_open($host, $username, $password);
+	}
+
+	/**
+	 * SQLiteAdapter::query()
+	 * 
+	 * @param mixed $query
+	 * @return
+	 */
+	public function query($query)
+	{
+		$this->result = sqlite_query($query, $this->connection);
+		return($this->result);
+	}
+
+	/**
+	 * SQLiteAdapter::fetchOne()
+	 * 
+	 * @param bool $query
+	 * @return
+	 */
+	public function fetchOne($query=false)
+	{	
+		if ($query != false) $this->query($query);
+		if($this->result !== false)
+		{
+			return( sqlite_fetch_single($this->result) );
+		}
+		return false;
+	}
+
+	/**
+	 * SQLiteAdapter::fetchRow()
+	 * 
+	 * @param bool $query
+	 * @param string $type
+	 * @return
+	 */
+	public function fetchRow($query=false, $type='assoc')
+	{
+		if ($query != false) $this->query($query);
+		if($this->result !== false)
+		{
+			$func = "sqlite_fetch_{$type}";
+			return( $func($this->result, 0));
+		}
+		return false;
+	}
+
+	/**
+	 * SQLiteAdapter::fetchAll()
+	 * 
+	 * @param bool $query
+	 * @param string $type
+	 * @return
+	 */
+	public function fetchAll($query=false, $type='assoc')
+	{
+		if ($query != false) $this->query($query);
+		if($this->result !== false)
+		{
+			if($type == 'assoc') return(sqlite_fetch_all($this->result, SQLITE_ASSOC));
+			if($type == 'object') 
+			{
+				$output = array();	
+				while ($row = sqlite_fetch_object($this->result))
+				{
+					$output[] = $row;
+				}
+			}
+			return $output;
+		}
+		return false;
+	}
+
+	/**
+	 * SQLiteAdapter::getInsertID()
+	 * 
+	 * @return
+	 */
+	public function getInsertID()
+	{
+		return sqlite_last_insert_rowid($this->connection);
+	}
+	
+	/**
+	 * SQLiteAdapter::numRows()
+	 * 
+	 * @return
+	 */
+	public function numRows()
+	{
+		return ($this->result) ? mysql_num_rows($this->result) : 0;
+	}
+
+	/**
+	 * SQLiteAdapter::numAffected()
+	 * 
+	 * @return
+	 */
+	public function numAffected()
+	{
+		return ($this->result) ? @mysql_affected_rows($this->result) : 0;
+	}
+
+	/**
+	 * SQLiteAdapter::selectDatabase()
+	 * 
+	 * @param mixed $db
+	 * @return
+	 */
+	public function selectDatabase($db)
+	{
+		return $this->connect($db);
+	}
+
+	/**
+	 * SQLiteAdapter::getError()
+	 * 
+	 * @return
+	 */
+	public function getError()
+	{
+		return sqlite_error_string(sqlite_last_error($this->connection));
+
+	}
+
+	/**
+	 * SQLiteAdapter::tableExists()
+	 * 
+	 * @param mixed $table
+	 * @return
+	 */
+	public function tableExists($table)
+	{
+		$input = $this->fetchOne("SELECT count(name) FROM sqlite_master WHERE type='table' and name='{$table}'");
+		return($input == 1);
+	}
+}
+
+
+
